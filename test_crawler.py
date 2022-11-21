8f1016568cb20e5d4eb97ea8ab1b1b98115808e8
Azret Anzorov <32574376+vavilon2142@users.noreply.github.com>
2022-11-21 15:25:41 +0300

    Update test_crawler.py

--- test_crawler.py
+++ test_crawler.py
@@ -1,6 +1,7 @@
 from web_crawler import Crawler
 
-url = "https://www.gov.uk"
+#@yourvar = {'User-Agent':''}
+url = " "
 start_anchor = "/"
 urls = Crawler.crawl(url, start_anchor)
 print(len(urls), urls)
